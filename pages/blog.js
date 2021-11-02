f3d48ae4027a7c4f9d6aa45c77e1db35170a624f
vallerydelexy <vallerydelexy@gmail.com>
2021-11-02 21:18:52 +0700

    entry 3

--- pages/blog.js
+++ pages/blog.js
@@ -20,6 +20,14 @@ const posts = [
         tags: ['javascript', 'beginners', 'webdev', 'codenewbie'],
         date: '1 Nov 2021',
         preview: 'API adalah singkatan dari Application Programming Interface, yang merupakan perantara perangkat lunak yang memungkinkan dua aplikasi untuk berbicara satu sama lain!',
+      },
+      {
+        id: 3,
+        url: '/blog/programmers-joke',
+        title: 'Programmers joke',
+        tags: ['jokes', 'programmers'],
+        date: '2 Nov 2021',
+        preview: 'no place like 127.0.0.1',
       }
   ]
   
