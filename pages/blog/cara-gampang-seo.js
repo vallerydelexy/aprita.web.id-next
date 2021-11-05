7c1c259019fae324f36f4302f7aa33d4daa95aac
rizki aprita <rizkiaprita.web@gmail.com>
2021-11-05 16:54:32 +0700

    ganti bahasa indonesia

--- pages/blog/cara-gampang-seo.js
+++ pages/blog/cara-gampang-seo.js
@@ -1,30 +1,171 @@
-import Head from 'next/head'
+import Head from "next/head";
 import Footer from "@components/Footer";
 import Header from "@components/Header";
-import PostPattern from "@components/PostPattern"
+import PostPattern from "@components/PostPattern";
 export default function caraGampangSeo() {
   return (
-<div className="relative pb-16 bg-white overflow-hidden">
+    <div className="relative pb-16 bg-white overflow-hidden">
       <Header />
       <Head>
         <title>7 API Dummy bagus</title>
         <link rel="icon" href="/favicon.svg" />
       </Head>
       <PostPattern />
-Apr 29 - <p>SEO is one of those topics that intimidate most marketers for the simple fact that the equation changes so frequently. One minute you think you have cracked the code and poof! Google comes out with a new algorithm and you must re-learn and adjust accordingly. Essentially, little about SEO is genuinely clear cut. So how do you optimize your website for search engines reliably when the only constant is change?</p>
-<p><strong>Here is a simple rule of thumb: Think of search engines as if they were just another website visitor.</strong></p>
-<p>Site visitors want fresh, relevant content and the best user experience possible. Search engines want to direct people to fresh, relevant content and good user experiences. Satisfy the needs of one, and by default you satisfy the needs of both. If you optimize your website for good user experiences, then it matters little if new algorithms are introduced. Quality content, content frequency, content organization, and content find-ability and share-ability all matter. Keeping that in mind, here are a few essential things you can do to improve the user experience that will in turn increase crawl frequency and improve search engine rankings.</p>
-<p><strong>Quality Content</strong><br />
-You’ve heard that content is king before, but here’s why that is even more true today. High reader interest and more engagement with content (comments, social sharing, etc.) is the single highest indicator of relevancy for search engines. Especially in today’s saturated market, as companies try to find more ways to lure in site visitors, user engagement is the new norm. Posting quick and dirty content will no longer cut it. Frequency is important, yes, but great content is the key here. It must be fresh, original, high quality, keyword-rich, and it must be valuable to website visitors. The more valuable and regularly updated the content, the more site visitors and search engines will visit. This provides a better website experience which search engines like.</p>
-<p>To have a continuous flow of great content your best bet is to blog, and frequently. Blog posts create a better website experience, help your website get indexed more often, encourage social sharing and conversation, establish expertise and credibility, and when shared afar blog posts create valuable inbound links. All of these factors push your site up in search results.</p>
-<p>Content is the center of all mobile sites as well, but more importantly, mobile visitors are social and are more apt to recommend your company socially if they have a good mobile experience. The more frequent your content, the more mobile visitors will visit and recommend your company using their favorite social media network if you give them one-click options for sharing.</p>
-<p><strong>Sharing Content</strong><br />
-Social media is not only the best way to promote the quality content you have created, it also creates better website engagement for site visitors who want to like, comment on, and share it. The good news is search engines now use social signals to determine how popular your content is by measuring Facebook likes, re-tweets, etc. Keyword-rich content, when promoted via social media in a coordinated keyword-rich campaign, can improve your search engine rankings quickly if you garner enough shares. The key is to make it easy for content contributors and site visitors to share your content across their preferred social channels.</p>
-<p><strong>Finding Content</strong><br />
-Here is a simple principle: if your navigation is confusing to site visitors, it‘s confusing to search engines. Creating easily crawlable navigation is less about categories and more about understanding what your site visitors are looking for and revealing the easiest path to find it. Overwhelming site visitors with tons of pages is confusing and doesn’t help the user experience or search engine rankings. You need to make sure the <em>right</em> pages are being crawled rather than all of your pages. Navigation— whether top or side, mega menus or accordions, breadcrumbs, sitemaps, etc.—should show site visitors where to go and the order of importance to ensure great web experiences, make sites more crawlable, and give a valuable boost to search engine confidence.</p>
-<p><strong>Labeling Content</strong><br />
-As I said before, content is still king, so the most important thing you can do is create impactful content leveraging topic-focused keywords and keyword phrases in your content. But that isn’t the whole story. Using keywords in links, human readable URLs, navigation, title tags, image and video tags and descriptions, and so on is just as essential to the user experience. The key is coordination. Your keywords must work in concert to create the best user experience. Properly optimizing your content for people with disabilities also improves search optimization. Labeling images and videos with alt-text for example allows visually-impaired people using screen readers to “read” your website. Search engine crawlers read websites in the same way, so accessibility improves search results.</p>
-<p>I could go on forever about this topic, but I’ll stop here. If you take one thing away from this let it be that if you create compelling web experiences for site visitors, you are well on your way to having an SEO optimized website.&nbsp;</p>
-<p>For more information on CommonSpot’s suite of SEO tools, click here.</p>
-<Footer /></div>  );
+      <div className="relative pt-8 px-4 sm:px-6 lg:px-8">
+        <p>
+          SEO adalah salah satu topik yang mengintimidasi sebagian besar pemasar
+          karena fakta sederhana bahwa persamaan sering berubah. Satu menit Anda
+          pikir Anda telah memecahkan kode dan poof! Google keluar dengan
+          algoritma baru dan Anda harus belajar kembali dan menyesuaikannya.
+          Pada dasarnya, sedikit tentang SEO yang benar-benar jelas. Jadi
+          bagaimana Anda mengoptimalkan situs web Anda untuk mesin pencari
+          dengan andal ketika satu-satunya konstanta adalah perubahan?
+        </p>
+        <section className="py-4">
+          <p>
+            <strong>
+              Berikut adalah aturan praktisnya: Pikirkan mesin pencari
+              seolah-olah mereka hanyalah pengunjung situs web lain.
+            </strong>
+          </p>
+          <p>
+            Pengunjung situs menginginkan konten yang segar dan relevan serta
+            pengalaman pengguna terbaik. Mesin pencari ingin mengarahkan orang
+            ke konten yang segar dan relevan, serta pengalaman pengguna yang
+            baik. Puaskan kebutuhan salah satu, dan secara default Anda memenuhi
+            kebutuhan keduanya. Jika Anda mengoptimalkan situs web Anda untuk
+            pengalaman pengguna yang baik, maka tidak masalah jika algoritme
+            baru diperkenalkan. Konten berkualitas, frekuensi konten, organisasi
+            konten, dan kemampuan menemukan konten dan kemampuan berbagi
+            semuanya penting. Dengan mengingat hal itu, berikut adalah beberapa
+            hal penting yang dapat Anda lakukan untuk meningkatkan pengalaman
+            pengguna yang pada gilirannya akan meningkatkan frekuensi perayapan
+            dan meningkatkan peringkat mesin telusur.
+          </p>
+        </section>
+
+        <section className="py-4">
+          <p>
+            <strong>Konten Berkualitas</strong>
+            <br />
+            Anda pernah mendengar bahwa konten adalah raja sebelumnya, tetapi
+            inilah mengapa hal itu bahkan lebih benar hari ini. Minat pembaca
+            yang tinggi dan lebih banyak keterlibatan dengan konten (komentar,
+            berbagi sosial, dll.) adalah satu-satunya indikator relevansi
+            tertinggi untuk mesin telusur. Terutama di pasar jenuh saat ini,
+            karena perusahaan mencoba menemukan lebih banyak cara untuk memikat
+            pengunjung situs, keterlibatan pengguna adalah norma baru.
+            Memposting konten yang cepat dan kotor tidak akan lagi memotongnya.
+            Frekuensi memang penting, tetapi konten yang bagus adalah kuncinya
+            di sini. Itu harus segar, asli, berkualitas tinggi, kaya kata kunci,
+            dan harus bernilai bagi pengunjung situs web. Semakin bernilai dan
+            konten yang diperbarui secara teratur, semakin banyak pengunjung
+            situs dan mesin pencari yang akan mengunjungi. Ini memberikan
+            pengalaman situs web yang lebih baik yang disukai mesin pencari.
+          </p>
+          <p>
+            Untuk memiliki aliran konten hebat yang berkelanjutan, taruhan
+            terbaik Anda adalah membuat blog, dan sering. Posting blog
+            menciptakan pengalaman situs web yang lebih baik, membantu situs web
+            Anda lebih sering diindeks, mendorong berbagi dan percakapan sosial,
+            membangun keahlian dan kredibilitas, dan ketika dibagikan jauh,
+            posting blog membuat tautan masuk yang berharga. Semua faktor ini
+            mendorong situs Anda ke atas dalam hasil pencarian.
+          </p>
+          <p>
+            Konten juga merupakan pusat dari semua situs seluler, tetapi yang
+            lebih penting, pengunjung seluler bersifat sosial dan lebih
+            cenderung merekomendasikan perusahaan Anda secara sosial jika mereka
+            memiliki pengalaman seluler yang baik. Semakin sering konten Anda,
+            semakin banyak pengunjung seluler akan mengunjungi dan
+            merekomendasikan perusahaan Anda menggunakan jaringan media sosial
+            favorit mereka jika Anda memberi mereka opsi sekali klik untuk
+            berbagi.
+          </p>
+        </section>
+
+        <section className="py-4">
+          <p>
+            <strong>Berbagi Konten</strong>
+            <br />
+            Media sosial bukan hanya cara terbaik untuk mempromosikan konten
+            berkualitas yang Anda buat, tetapi juga menciptakan keterlibatan
+            situs web yang lebih baik bagi pengunjung situs yang ingin menyukai,
+            mengomentari, dan membagikannya. Kabar baiknya adalah mesin pencari
+            sekarang menggunakan sinyal sosial untuk menentukan seberapa populer
+            konten Anda dengan mengukur suka Facebook, tweet ulang, dll. Konten
+            kaya kata kunci, ketika dipromosikan melalui media sosial dalam
+            kampanye kaya kata kunci yang terkoordinasi, dapat meningkatkan
+            pencarian Anda peringkat mesin dengan cepat jika Anda mengumpulkan
+            cukup banyak saham. Kuncinya adalah memudahkan kontributor konten
+            dan pengunjung situs untuk membagikan konten Anda di saluran sosial
+            pilihan mereka.
+          </p>
+        </section>
+
+        <section className="py-4">
+          <p>
+            <strong>Finding Content</strong>
+            <br />
+            Here is a simple principle: if your navigation is confusing to site
+            visitors, it‘s confusing to search engines. Creating easily
+            crawlable navigation is less about categories and more about
+            understanding what your site visitors are looking for and revealing
+            the easiest path to find it. Overwhelming site visitors with tons of
+            pages is confusing and doesn’t help the user experience or search
+            engine rankings. You need to make sure the <em>right</em> pages are
+            being crawled rather than all of your pages. Navigation— whether top
+            or side, mega menus or accordions, breadcrumbs, sitemaps,
+            etc.—should show site visitors where to go and the order of
+            importance to ensure great web experiences, make sites more
+            crawlable, and give a valuable boost to search engine confidence.
+          </p>
+        </section>
+
+        <section className="py-4">
+          <p>
+            <strong>Menemukan Konten</strong>
+            <br />
+            Berikut adalah prinsip sederhana: jika navigasi Anda membingungkan
+            pengunjung situs, itu membingungkan mesin pencari. Membuat navigasi
+            yang mudah dirayapi bukanlah tentang kategori dan lebih banyak
+            tentang memahami apa yang dicari pengunjung situs Anda dan
+            mengungkapkan jalur termudah untuk menemukannya. Pengunjung situs
+            yang luar biasa dengan banyak halaman membingungkan dan tidak
+            membantu pengalaman pengguna atau peringkat mesin pencari. Anda
+            perlu memastikan halaman yang tepat sedang dirayapi daripada semua
+            halaman Anda. Navigasi— baik atas atau samping, menu mega atau
+            akordeon, remah roti, peta situs, dll.—harus menunjukkan kepada
+            pengunjung situs ke mana harus pergi dan urutan kepentingan untuk
+            memastikan pengalaman web yang hebat, membuat situs lebih dapat
+            dirayapi, dan memberikan dorongan yang berharga untuk mesin telusur
+            kepercayaan diri.
+          </p>
+        </section>
+
+        <section className="py-4">
+          <p>
+            <strong>Memberi Label Konten</strong>
+            <br />
+            Seperti yang saya katakan sebelumnya, konten masih raja, jadi hal
+            terpenting yang dapat Anda lakukan adalah membuat konten yang
+            berdampak dengan memanfaatkan kata kunci dan frasa kata kunci yang
+            berfokus pada topik dalam konten Anda. Tapi itu bukan keseluruhan
+            cerita. Menggunakan kata kunci dalam tautan, URL yang dapat dibaca
+            manusia, navigasi, tag judul, tag dan deskripsi gambar dan video,
+            dan sebagainya sama pentingnya untuk pengalaman pengguna. Kuncinya
+            adalah koordinasi. Kata kunci Anda harus bekerja bersama untuk
+            menciptakan pengalaman pengguna terbaik. Mengoptimalkan konten Anda
+            dengan benar untuk penyandang disabilitas juga meningkatkan
+            pengoptimalan pencarian. Memberi label pada gambar dan video dengan
+            teks alternatif, misalnya, memungkinkan orang dengan gangguan
+            penglihatan menggunakan pembaca layar untuk "membaca" situs web
+            Anda. Perayap mesin telusur membaca situs web dengan cara yang sama,
+            sehingga aksesibilitas meningkatkan hasil penelusuran.
+          </p>
+        </section>
+      </div>
+      <Footer />
+    </div>
+  );
 }
