52e8e4adf0ea9285480cdf3d09e01ceeed6ac9d5
Rizki Aprita <vallerydelexy@gmail.com>
2022-07-17 03:54:43 +0700

    yay

--- pages/blog.js
+++ pages/blog.js
@@ -3,7 +3,6 @@ import Footer from "@components/Footer";
 import Header from "@components/Header";
 import HeroPattern from '@components/HeroPattern';
 
-HeroPattern
 const posts = [
     {
       id: 1,
