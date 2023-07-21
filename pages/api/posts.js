77ed2c63e1fd6b0ca58c113e20da72dbbd4f8685
rizki aprita <vallerydelexy@gmail.com>
2023-07-21 18:59:37 +0700

    lets try this

--- pages/api/posts.js
+++ pages/api/posts.js
@@ -1,5 +1,5 @@
 import { testController } from "@api/controller/posts.controller"
 
-export default (req, res) => {       
-        return testController(req, res)
+export default async (req, res) => {       
+        return await testController(req, res)
   };
