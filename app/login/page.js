4dc433a91cd6acfbec9b0a98a74c7247b151697b
rizki aprita <vallerydelexy@gmail.com>
2023-08-01 06:46:57 +0700

    login btn

--- app/login/page.js
+++ app/login/page.js
@@ -4,40 +4,58 @@ import axios from "axios"
 import { setCookie, getCookie } from "cookies-next"
 import { useRouter } from "next/navigation"
 
+const API_ENDPOINT = `${process.env.NEXT_PUBLIC_API}/auth/login`;
+const DOMAIN = process.env.NEXT_PUBLIC_DOMAIN;
+
 export default function login() {
 	const [email, setEmail] = useState()
 	const [password, setPassword] = useState()
 	const router = useRouter()
-	async function sendCredential(e) {
-		e.preventDefault()
-		const jwt = await axios.post(`${process.env.NEXT_PUBLIC_API}/auth/login`, {
-			email: email,
-			password: password,
-		})
-
-		if (jwt) {
-			setCookie("jwt", jwt.data, { maxAge: 60 * 6 * 24 })
-			router.push(`${process.env.NEXT_PUBLIC_DOMAIN}/dashboard`)
+	const sendCredential = async () => {
+		try {
+		  const response = await axios.post(API_ENDPOINT, {
+			email,
+			password,
+		  });
+	
+		  const jwt = response.data;
+		  if (jwt) {
+			setCookie('jwt', jwt, { maxAge: 60 * 6 * 24 });
+			router.push(`${DOMAIN}/dashboard`);
+		  } else {
+			// Handle error when the response doesn't contain the expected data
+			console.error('Invalid response from the server');
+		  }
+		} catch (error) {
+		  // Handle error when the API request fails
+		  console.error('Failed to login:', error.message);
 		}
-	}
+	  };
+
+	const handleKeyDown = (e) => {
+		e.preventDefault()
+		if (e.key === 'Enter') {
+		  sendCredential();
+		}
+	  };
 	return (
-		<div className="flex flex-col gap-4 p-4 max-w-screen-md mx-auto">
+		<main className="flex flex-col gap-4 p-4 max-w-screen-md mx-auto">
 			<input
 				type="email"
 				onChange={(e) => setEmail(e.target.value)}
 				className="dark:bg-gray-100 bg-gray-800 p-2 rounded-lg"
-				placeholder="email"></input>
+				placeholder="email"
+				onKeyDown={handleKeyDown} />
 			<input
 				type="password"
 				onChange={(e) => setPassword(e.target.value)}
 				className="dark:bg-gray-100 bg-gray-800 p-2 rounded-lg"
-				placeholder="password"></input>
-			<button
-				onClick={(e) => {
-					sendCredential(e)
-				}}>
+				placeholder="password"
+				onKeyDown={handleKeyDown} />
+			<button className="p-2 text-gray-800 dark:text-gray-200 bg-green-400 rounded-lg font-bold"
+				onClick={sendCredential}>
 				Login
 			</button>
-		</div>
+		</main>
 	)
 }
