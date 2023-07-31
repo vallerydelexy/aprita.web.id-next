f53129818d9403bd5a664d4bec157dd1666bbbda
rizki aprita <vallerydelexy@gmail.com>
2023-08-01 05:34:46 +0700

    dark is default

--- utils/store/theme.store.js
+++ utils/store/theme.store.js
@@ -1,7 +1,7 @@
 import { create } from "zustand";
 
 const useThemeStore = create((set) => ({
-  mode: "light",
+  mode: "dark",
   setLightMode: () =>
     set((state) => {
       localStorage.theme = "light";
