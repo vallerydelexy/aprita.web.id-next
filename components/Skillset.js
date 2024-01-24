7b9404599c313f18c2a3a64f7518ef9d502f9153
Rizki Aprita <vallerydelexy@gmail.com>
2024-01-24 08:24:33 +0700

    add laravel

--- components/Skillset.js
+++ components/Skillset.js
@@ -37,9 +37,10 @@ export default function Skillset () {
         <div className="py-4 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
           <dt className="text-sm font-medium text-gray-500 dark:text-gray-100">Backend</dt>
           <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+          <Badge text={"Laravel"} />
+          <Badge text={"Code Igniter"} />
             <Badge text={"Express JS"} />
             <Badge text={"Koa JS"} />
-            <Badge text={"Code Igniter"} />
           </dd>
         </div>
         <div className="py-4 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
